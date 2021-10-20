465a4195367036314def9d5ca3304ac71d9759ed
João Vítor Carvalho <mistingerr@gmail.com>
2021-10-20 13:12:08 -0300

    added-chessboard

---
+++ csharp-chess/Board/ChessBoard.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_chess.Board
+{
+    class ChessBoard
+    {
+        public int Lines { get; set; }
+        public int Colunms { get; set; }
+
+        private Piece[,] Pieces;
+
+        public ChessBoard(int lines, int colunms)
+        {
+            Lines = lines;
+            Colunms = colunms;
+            Pieces = new Piece[Lines, Colunms];
+        }
+
+        
+    }
+}
---
+++ csharp-chess/Board/Piece.cs
@@ -0,0 +1,25 @@
+﻿using Board;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_chess.Board
+{
+    class Piece
+    {
+        public Position Position { get; set; }
+        public Color Color { get; protected set; }
+        public int QntyMoves { get; protected set; }
+        public ChessBoard Brd { get; protected set; }
+
+        public Piece(Position position, Color color, ChessBoard brd)
+        {
+            Position = position;
+            Color = color;
+            Brd = brd;
+            QntyMoves = 0;
+        }
+    }
+}
